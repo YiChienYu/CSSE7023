5b12908ac27c66a51926dc3652c508d56ff0b4a4
s4606373 <s4606373@36b3bf84-f2f4-a54a-86e1-41d0d6d76067>
2020-10-22 06:44:27 +0000

    RuleBasedHazardEvaluator fixed

--- ass2/src/bms/building/BuildingInitialiser.java
+++ ass2/src/bms/building/BuildingInitialiser.java
@@ -24,10 +24,10 @@ public class BuildingInitialiser {
      *
      * @param filename path of the file from which to load a list of buildings
      * @return a list containing all the buildings loaded from the file
-     * @throws IOException         if an IOException is encountered when calling any
-     *                             IO methods
+     * @throws IOException if an IOException is encountered when calling any
+     * IO methods
      * @throws FileFormatException if the file format of the given file
-     *                             is invalid according to the rules above
+     * is invalid according to the rules above
      */
     public static List<Building> loadBuildings(String filename) throws
             IOException, FileFormatException {
